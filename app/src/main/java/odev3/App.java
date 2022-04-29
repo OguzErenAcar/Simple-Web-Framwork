551fcd0d03598e49612368392fa965e30f83207d
oguz <oguzacrern@gmail.com>
2022-04-29 03:52:26 +0300

    ...

--- app/src/main/java/odev3/App.java
+++ app/src/main/java/odev3/App.java
@@ -21,42 +21,44 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
-    /*   get("/compute", (req, res) -> "Hello World");  */
+        get("/compute", (req, res) -> "Hello World");
 
-    Logger logger =LogManager.getLogger(App.class);
-    logger.error("message");
+        /*
+         * Logger logger =LogManager.getLogger(App.class);
+         * logger.error("message");
+         * 
+         * get("/compute",
+         * (rq, rs) -> {
+         * Map<String, String> map = new HashMap<String, String>();
+         * map.put("result", "not computed yet!");
+         * return new ModelAndView(map, "compute.mustache");
+         * },
+         * new MustacheTemplateEngine());
+         * 
+         * post("/compute", // url
+         * (req, res) -> {
+         * String input1 = req.queryParams("input1");
+         * java.util.Scanner sc1 = new java.util.Scanner(input1);
+         * sc1.useDelimiter("[;\r\n]+");
+         * ArrayList<Integer> inputList = new ArrayList<>();
+         * while (sc1.hasNext()) {
+         * int value = Integer.parseInt(sc1.next().replaceAll("\\s", ""));
+         * inputList.add(value);
+         * }
+         * sc1.close();
+         * System.out.println(inputList);
+         * 
+         * String input2 = req.queryParams("input2").replaceAll("\\s", "");
+         * int input2AsInt = Integer.parseInt(input2);
+         * 
+         * boolean result = App.search(inputList,input2AsInt);
+         * Map<String, Boolean> map = new HashMap<String, Boolean>();
+         * map.put("result", result);
+         * return new ModelAndView(map, "compute.mustache");
+         * },
+         * new MustacheTemplateEngine());
+         */
 
-      get("/compute",  
-                (rq, rs) -> {
-                    Map<String, String> map = new HashMap<String, String>();
-                    map.put("result", "not computed yet!");
-                    return new ModelAndView(map, "compute.mustache");
-                },
-                new MustacheTemplateEngine());
-
-          post("/compute", // url
-                (req, res) -> {
-                    String input1 = req.queryParams("input1");
-                    java.util.Scanner sc1 = new java.util.Scanner(input1);
-                    sc1.useDelimiter("[;\r\n]+");
-                    ArrayList<Integer> inputList = new ArrayList<>();
-                    while (sc1.hasNext()) {
-                        int value = Integer.parseInt(sc1.next().replaceAll("\\s", ""));
-                        inputList.add(value);
-                    }
-                    sc1.close();
-                    System.out.println(inputList);
-
-                    String input2 = req.queryParams("input2").replaceAll("\\s", "");
-                    int input2AsInt = Integer.parseInt(input2);
-
-                    boolean result = App.search(inputList,input2AsInt);
-                    Map<String, Boolean> map = new HashMap<String, Boolean>();
-                    map.put("result", result);
-                    return new ModelAndView(map, "compute.mustache");
-                },
-                new MustacheTemplateEngine());
- 
     }
 
     public static boolean search(ArrayList<Integer> array, int e) {
@@ -69,5 +71,5 @@ public class App {
         }
         return false;
     }
-    
+
 }
